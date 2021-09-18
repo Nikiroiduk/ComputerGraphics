754cd858f712654be91960714c15f7f241c45534
Niki <Nikita.Isuk@outlook.com>
2021-09-18 19:53:07 +0300

    Lab 2 complete

--- lab1_CreatingGraphicalWindowsApplication/Form1.cs
+++ lab1_CreatingGraphicalWindowsApplication/Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace lab1_CreatingGraphicalWindowsApplication
@@ -16,5 +9,25 @@ namespace lab1_CreatingGraphicalWindowsApplication
         {
             InitializeComponent();
         }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawLine(Pens.Red, 10, 5, 110, 15);
+            g.DrawEllipse(Pens.Blue, 10, 20, 110, 45);
+            g.DrawRectangle(Pens.Green, 10, 70, 110, 45);
+            g.FillEllipse(Brushes.Blue, 130, 20, 110, 45);
+            g.FillRectangle(Brushes.Green, 130, 70, 110, 45);
+            base.OnPaint(e);
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    Pen myPen = new Pen(Color.Red, 1);
+        //    Graphics g = pictureBox1.CreateGraphics();
+        //    g.DrawRectangle(myPen, 0, 0, pictureBox1.Size.Width - 1,
+        //    pictureBox1.Size.Height - 1);
+        //    g.Dispose();
+        //}
     }
 }
