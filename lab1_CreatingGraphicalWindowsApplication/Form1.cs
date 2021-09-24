88811fd4e86bd42c49f04da7480eb00aee0831e6
Niki <Nikita.Isuk@outlook.com>
2021-09-24 23:15:37 +0300

    meh

--- lab1_CreatingGraphicalWindowsApplication/Form1.cs
+++ lab1_CreatingGraphicalWindowsApplication/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace lab1_CreatingGraphicalWindowsApplication
@@ -10,24 +11,24 @@ namespace lab1_CreatingGraphicalWindowsApplication
             InitializeComponent();
         }
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-            g.DrawLine(Pens.Red, 10, 5, 110, 15);
-            g.DrawEllipse(Pens.Blue, 10, 20, 110, 45);
-            g.DrawRectangle(Pens.Green, 10, 70, 110, 45);
-            g.FillEllipse(Brushes.Blue, 130, 20, 110, 45);
-            g.FillRectangle(Brushes.Green, 130, 70, 110, 45);
-            base.OnPaint(e);
-        }
-
-        //private void button1_Click(object sender, EventArgs e)
+        //protected override void OnPaint(PaintEventArgs e)
         //{
-        //    Pen myPen = new Pen(Color.Red, 1);
-        //    Graphics g = pictureBox1.CreateGraphics();
-        //    g.DrawRectangle(myPen, 0, 0, pictureBox1.Size.Width - 1,
-        //    pictureBox1.Size.Height - 1);
-        //    g.Dispose();
+        //    Graphics g = e.Graphics;
+        //    g.DrawLine(Pens.Red, 10, 5, 110, 15);
+        //    g.DrawEllipse(Pens.Blue, 10, 20, 110, 45);
+        //    g.DrawRectangle(Pens.Green, 10, 70, 110, 45);
+        //    g.FillEllipse(Brushes.Blue, 130, 20, 110, 45);
+        //    g.FillRectangle(Brushes.Green, 130, 70, 110, 45);
+        //    base.OnPaint(e);
         //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen myPen = new Pen(Color.Red, 1);
+            Graphics g = pictureBox1.CreateGraphics();
+            g.DrawRectangle(myPen, 0, 0, pictureBox1.Size.Width - 1,
+            pictureBox1.Size.Height - 1);
+            g.Dispose();
+        }
     }
 }
