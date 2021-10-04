22d82316f44351c2df10957aa424ba3ae79de35b
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2021-10-04 15:23:12 +0300

    complete: lab4

---
+++ lab4_LineDrawing/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace lab4_LineDrawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Draw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(960, 468);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Draw);
+            this.groupBox1.Location = new System.Drawing.Point(734, 486);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 0, 6, 6);
+            this.groupBox1.Size = new System.Drawing.Size(238, 63);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // Draw
+            // 
+            this.Draw.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Draw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Draw.Location = new System.Drawing.Point(6, 13);
+            this.Draw.Margin = new System.Windows.Forms.Padding(3, 3, 3, 8);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(226, 44);
+            this.Draw.TabIndex = 0;
+            this.Draw.TabStop = false;
+            this.Draw.Text = "Draw";
+            this.Draw.UseVisualStyleBackColor = true;
+            this.Draw.Click += new System.EventHandler(this.Draw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Draw;
+    }
+}
+
---
+++ lab4_LineDrawing/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab4_LineDrawing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string CreateDragonLine()
+        {
+            int orderCurve = 7;
+            string str = "1";
+            for (int i = 1; i < orderCurve; i++)
+            {
+                StringBuilder sb = new StringBuilder(str);
+                int midle = (int)Math.Floor((double)sb.Length / 2);
+                sb[midle] = '0';
+                str = str + "1" + sb;
+            }
+            return str;
+        }
+
+        private void Draw_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            Pen p = new Pen(Color.FromArgb(255, 128, 0), 20);
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            p.CompoundArray = new float[] { 0.0f, 0.1f, 0.3f, 0.5f, 0.8f, 1.0f};
+
+            Pen dragonPen = new Pen(Color.FromArgb(70, 70, 70), 2);
+            int dx = 23;
+
+            string str = CreateDragonLine();
+            int x1 = pictureBox1.Size.Width / 2;
+            int y1 = pictureBox1.Size.Height / 2;
+            int x2 = pictureBox1.Size.Width / 2;
+            int y2 = pictureBox1.Size.Height / 2 - dx;
+            int x3 = x2;
+            int y3 = y2;
+            g.DrawLine(dragonPen, x1, y1, x3, y3);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (y2 - y1 < 0)
+                {
+                    if (str[i] == '1') x3 = x2 - dx;
+                    else x3 = x2 + dx;
+                    y3 = y2;
+                }
+                if (x2 - x1 < 0)
+                { 
+                    if (str[i] == '1') y3 = y2 - dx;
+                    else y3 = y2 + dx; 
+                    x3 = x2;
+                }
+                if (x2 - x1 > 0)
+                { 
+                    if (str[i] == '1') y3 = y2 + dx;
+                    else y3 = y2 - dx;
+                    x3 = x2;
+                }
+                if (y2 - y1 > 0)
+                { 
+                    if (str[i] == '1') x3 = x2 + dx;
+                    else x3 = x2 - dx;
+                    y3 = y2;
+                }
+                if (i == str.Length - 1)
+                {
+                    g.DrawLine(p, x2, y2, x3, y3);
+                }
+                else
+                {
+                    g.DrawLine(dragonPen, x2, y2, x3, y3);
+                }
+                x1 = x2; y1 = y2;
+                x2 = x3; y2 = y3;
+            }
+        }
+    }
+}
