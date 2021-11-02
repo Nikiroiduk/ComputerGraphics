560d5500bd2538a5db56d0c39c0529a5e7fe71e0
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2021-11-03 00:40:15 +0300

    started: lab7

---
+++ lab7_BuildingDynamicImages/lab7_BuildingDynamicImages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7_BuildingDynamicImages
+{
+    public partial class lab7_BuildingDynamicImages : Form
+    {
+        public lab7_BuildingDynamicImages()
+        {
+            InitializeComponent();
+        }
+    }
+}
